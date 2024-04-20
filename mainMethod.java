9feb7b52da53051934913aa555683500f30e2297
11222687 <rahuljaikar7676r@gmail.com>
2024-04-20 23:54:18 +0530

    first commit

---
+++ mainMethod.java
@@ -0,0 +1,5 @@
+class mainMethod{
+public static void main(String[] rahul){
+System.out.println("hi, i am rahul");
+}
+}
---
+++ student.java
@@ -0,0 +1,16 @@
+class student{
+String name;
+int id;
+int age;
+
+
+public static void main(String[] rahul){
+student s=new student();
+s.name="rahul";
+s.id=11222687;
+s.age=21;
+
+System.out.println(s.name+"  "+s.id);
+
+}
+}
